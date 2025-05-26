8ca6389463c37c0a4868fa85d756534859b0345b
ShinjungOh <tlswjd9260@gmail.com>
2025-05-26 13:22:57 +0900

    Updates

--- assets/index-D45AAUEc.js
+++ assets/index-D45AAUEc.js
@@ -58,6 +58,55 @@ function _mergeNamespaces(n2, m2) {
     fetch(link.href, fetchOpts);
   }
 })();
+const scriptRel = "modulepreload";
+const assetsURL = function(dep) {
+  return "/react-shopping-products/" + dep;
+};
+const seen = {};
+const __vitePreload = function preload(baseModule, deps, importerUrl) {
+  let promise = Promise.resolve();
+  if (deps && deps.length > 0) {
+    document.getElementsByTagName("link");
+    const cspNonceMeta = document.querySelector("meta[property=csp-nonce]");
+    const cspNonce = (cspNonceMeta == null ? void 0 : cspNonceMeta.nonce) || (cspNonceMeta == null ? void 0 : cspNonceMeta.getAttribute("nonce"));
+    promise = Promise.all(deps.map((dep) => {
+      dep = assetsURL(dep);
+      if (dep in seen)
+        return;
+      seen[dep] = true;
+      const isCss = dep.endsWith(".css");
+      const cssSelector = isCss ? '[rel="stylesheet"]' : "";
+      if (document.querySelector(`link[href="${dep}"]${cssSelector}`)) {
+        return;
+      }
+      const link = document.createElement("link");
+      link.rel = isCss ? "stylesheet" : scriptRel;
+      if (!isCss) {
+        link.as = "script";
+        link.crossOrigin = "";
+      }
+      link.href = dep;
+      if (cspNonce) {
+        link.setAttribute("nonce", cspNonce);
+      }
+      document.head.appendChild(link);
+      if (isCss) {
+        return new Promise((res, rej) => {
+          link.addEventListener("load", res);
+          link.addEventListener("error", () => rej(new Error(`Unable to preload CSS for ${dep}`)));
+        });
+      }
+    }));
+  }
+  return promise.then(() => baseModule()).catch((err) => {
+    const e2 = new Event("vite:preloadError", { cancelable: true });
+    e2.payload = err;
+    window.dispatchEvent(e2);
+    if (!e2.defaultPrevented) {
+      throw err;
+    }
+  });
+};
 function getDefaultExportFromCjs(x2) {
   return x2 && x2.__esModule && Object.prototype.hasOwnProperty.call(x2, "default") ? x2["default"] : x2;
 }
@@ -10364,7 +10413,7 @@ function Modal({
     )
   ] });
 }
-const baseUrl = "https://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com";
+const baseUrl = "http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com";
 const fetchWithAuth = async (endpoint, options = {}, errorMessage) => {
   const url = `${baseUrl}${endpoint}`;
   const defaultHeaders = {
@@ -10847,9 +10896,10 @@ function App() {
   ] });
 }
 async function enableMocking() {
-  {
-    return;
-  }
+  const { worker } = await __vitePreload(() => import("./browser-CWzRlfeu.js"), true ? [] : void 0);
+  return worker.start({
+    onUnhandledRequest: "bypass"
+  });
 }
 enableMocking().then(() => {
   client.createRoot(document.getElementById("root")).render(
